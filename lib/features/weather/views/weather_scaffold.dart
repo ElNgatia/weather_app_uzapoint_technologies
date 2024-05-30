0a7dbe96fd828d936239e85c4354ff9e54cb9cb4
ElNgatia <ngatiadyte@gmail.com>
2024-05-30 06:16:38 +0300

    feat: Add connectivity_plus dependency for network connectivity handling

--- lib/features/weather/views/weather_scaffold.dart
+++ lib/features/weather/views/weather_scaffold.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,6 +15,22 @@ class WeatherHomeScreen extends StatefulWidget {
 }
 
 class _WeatherHomeScreenState extends State<WeatherHomeScreen> {
+  late Future<bool> _connectivityFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _connectivityFuture = checkInternetConnectivity();
+  }
+
+  Future<bool> checkInternetConnectivity() async {
+    final List<ConnectivityResult> result =
+        await (Connectivity().checkConnectivity());
+
+    return result.contains(ConnectivityResult.mobile) ||
+        result.contains(ConnectivityResult.wifi);
+  }
+
   int _selectedIndex = 0;
 
   static const List<Widget> _screens = <Widget>[
@@ -27,44 +44,82 @@ class _WeatherHomeScreenState extends State<WeatherHomeScreen> {
     });
   }
 
+  void _retryConnectivityCheck() {
+    setState(() {
+      _connectivityFuture = checkInternetConnectivity();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Weather App'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const SearchPage();
-              }));
-            },
-          ),
-        ],
-      ),
-      body: _screens.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.wb_sunny),
-            label: 'Current Weather',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.cloud),
-            label: 'Forecast',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
-        },
-        child: const Icon(Icons.brightness_6),
-      ),
+    return FutureBuilder<bool>(
+      future: _connectivityFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else if (snapshot.hasError || !snapshot.data!) {
+          return Scaffold(
+            body: Center(
+              child: Column(
+                children: [
+                  const Text(
+                    'No Internet Connection',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  ElevatedButton(
+                    onPressed: _retryConnectivityCheck,
+                    child: const Text('Retry'),
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: const Text('Weather App'),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return const SearchPage();
+                    }));
+                  },
+                ),
+              ],
+            ),
+            body: _screens.elementAt(_selectedIndex),
+            bottomNavigationBar: BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.wb_sunny),
+                  label: 'Current Weather',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.cloud),
+                  label: 'Forecast',
+                ),
+              ],
+              currentIndex: _selectedIndex,
+              onTap: _onItemTapped,
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                Provider.of<ThemeProvider>(context, listen: false)
+                    .toggleTheme();
+              },
+              child: const Icon(Icons.brightness_6),
+            ),
+          );
+        }
+      },
     );
   }
 }
